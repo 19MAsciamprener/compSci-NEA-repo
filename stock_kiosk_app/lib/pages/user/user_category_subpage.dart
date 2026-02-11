dcbe59c8e176703ce7bff58b02e6a7d23a9d58a3
19MAsciamprener <19masciamprener@ashfieldstudent.ttct.co.uk>
2026-02-11 19:31:41 +0000

    Added category subpage
    needs work still

---
+++ stock_kiosk_app/lib/pages/user/user_category_subpage.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/material.dart';
+import 'package:stock_kiosk_app/pages/global/stock_price_page.dart';
+import 'package:stock_kiosk_app/pages/user/user_account_page.dart';
+import 'package:stock_kiosk_app/pages/user/user_purchase_page.dart';
+import 'package:stock_kiosk_app/widgets/home_page_widgets.dart';
+import 'package:stock_kiosk_app/widgets/profile_picture_widget.dart';
+import 'package:stock_kiosk_app/widgets/stock_list_widgets.dart';
+
+class UserCategorySubpage extends StatelessWidget {
+  const UserCategorySubpage({super.key, required this.category});
+  final String category;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 96,
+        title: const Text(''),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: GestureDetector(
+              onTap: () {
+                // tap profile picture to go to account page
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => UserAccountPage()),
+                );
+              },
+              child: ProfilePictureWidget(context, size: 96),
+            ),
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              GestureDetector(
+                //gesture detector to make whole stock container tappable
+                onTap: () {
+                  Navigator.push(
+                    //navigate to full stock price page on tap (allows user to return to standby page)
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => StockPricePage(),
+                    ), // TODO: MAKE STOCK PRICE PAGE
+                  );
+                },
+                child: MiniStockListBox(),
+              ),
+              const SizedBox(height: 16.0),
+
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(child: ItemCards(category: category)),
+
+                    SizedBox(width: 16.0),
+
+                    SizedBox(
+                      width: 220,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Container(
+                            margin: EdgeInsets.only(left: 16),
+                            padding: EdgeInsets.all(16),
+                            decoration: BoxDecoration(
+                              color: Colors.grey.shade800,
+                              borderRadius: BorderRadius.circular(16),
+                            ),
+                            height: 700,
+                            child: Column(
+                              children: [
+                                Text(
+                                  'Current Items',
+                                  textAlign: TextAlign.start,
+                                  style: Theme.of(context).textTheme.titleLarge,
+                                ),
+                                Container(
+                                  alignment: Alignment.centerRight,
+                                  child: Text(
+                                    'Location: Warehouse 1',
+                                    textAlign: TextAlign.end,
+                                    style: Theme.of(context).textTheme.bodySmall
+                                        ?.copyWith(color: Colors.white70),
+                                  ),
+                                ),
+                                SizedBox(height: 18),
+                                Expanded(child: CartItemList()),
+                                Divider(),
+
+                                Text(
+                                  'Total:', // TODO: MAKE DYNAMIC
+                                  style: Theme.of(
+                                    context,
+                                  ).textTheme.titleMedium,
+                                ),
+
+                                Text(
+                                  '\$123.45', // TODO: MAKE DYNAMIC
+                                  style: Theme.of(
+                                    context,
+                                  ).textTheme.titleMedium,
+                                ),
+
+                                SizedBox(height: 18),
+
+                                ElevatedButton(
+                                  style: Theme.of(context)
+                                      .elevatedButtonTheme
+                                      .style
+                                      ?.copyWith(
+                                        textStyle: WidgetStatePropertyAll(
+                                          TextStyle(fontSize: 24),
+                                        ),
+                                      ), // make purchase button smaller than category buttons
+                                  onPressed: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) =>
+                                            UserPurchasePage(),
+                                      ),
+                                    );
+                                  },
+                                  child: Text('Purchase'), // TODO: MAKE DYNAMIC
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- stock_kiosk_app/lib/widgets/home_page_widgets.dart
+++ stock_kiosk_app/lib/widgets/home_page_widgets.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:stock_kiosk_app/pages/user/user_category_subpage.dart';
 
 class CategoryCards extends StatelessWidget {
   const CategoryCards({super.key});
@@ -23,7 +25,12 @@ class CategoryCards extends StatelessWidget {
         final category = categories[index];
         return GestureDetector(
           onTap: () {
-            //TODO: NAVIGATE TO CATEGORY PAGE
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => UserCategorySubpage(category: category),
+              ),
+            );
           },
           child: Card(
             color: Colors.grey,
@@ -81,6 +88,62 @@ class CartItemList extends StatelessWidget {
               ),
             ],
           ),
+        );
+      },
+    );
+  }
+}
+
+class ItemCards extends StatelessWidget {
+  const ItemCards({super.key, required this.category});
+  final String category;
+
+  @override
+  Widget build(BuildContext context) {
+    final query = FirebaseFirestore.instance
+        .collection('items')
+        .where('category', isEqualTo: category);
+
+    return StreamBuilder(
+      stream: query.snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+          return const Center(child: Text('No items found in this category.'));
+        }
+
+        final docs = snapshot.data!.docs;
+
+        return GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 16,
+            mainAxisSpacing: 16,
+            childAspectRatio: 1,
+          ),
+          itemCount: docs.length,
+          itemBuilder: (context, index) {
+            final doc = docs[index];
+            return GestureDetector(
+              onTap: () {
+                //TODO: ADD TO CART FUNCTIONALITY
+              },
+              child: Card(
+                color: Colors.grey,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                child: Center(
+                  child: Text(
+                    doc['name'],
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                ),
+              ),
+            );
+          },
         );
       },
     );
